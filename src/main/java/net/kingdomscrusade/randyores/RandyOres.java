945c88a297acc0f869598eb4f59744fc5e2d9dbc
LittleHillMYR <user@littlehillmyr.ga>
2021-06-29 14:49:56 +0800

    First commit :D!

---
+++ src/main/java/net/kingdomscrusade/randyores/RandyOres.java
@@ -0,0 +1,48 @@
+package net.kingdomscrusade.randyores;
+
+import lombok.Getter;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public final class RandyOres extends JavaPlugin {
+
+    @Getter
+    private static Plugin plugin;
+    @Getter
+    private static Economy econ;
+    private static final Logger log = Logger.getLogger("Minecraft");
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+        this.saveDefaultConfig(); // Setting up configs
+        if (!setupEconomy()) { // Setting up economy instance
+            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        getServer().getPluginManager().registerEvents(new BlockBreakListener(), this); // Setting up listener
+    }
+
+    @Override
+    public void onDisable() {
+        log.info(String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion()));
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return true;
+    }
+}
+
